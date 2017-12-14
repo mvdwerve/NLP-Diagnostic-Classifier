8868bada013308a2ae39675c5b3963461baf9c37
Michael van der Werve <mvdwerve@outlook.com>
2017-12-14 15:19:25 +0100

    new, cleaner version of main

--- main.py
+++ main.py
@@ -14,9 +14,11 @@ parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 0.01)')
 parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
                     help='SGD momentum (default: 0.5)')
+parser.add_argument('--batch_size', type=int, default=32,
+                    help='Batch size (default: 32)')
 args = parser.parse_args()
 
-softmax = nn.Softmax()
+# make the dictionaries, tags and words
 pos_dict = {}
 pos_tags = {}
 sentences = []
@@ -46,7 +48,7 @@ with open('ted_lm/to_run/data/train.txt') as f:
             sentences.append(' '.join(line.split(' ')[:-2]))
             target_words.append(line.split(' ')[-2])
 
-print(target_words)
+# load the model and dictionary into memory
 lm = torch.load('ted_lm/to_run/model-lr-10-min5.pt', map_location=lambda storage, loc: storage)
 dictionary = pickle.load(open('ted_lm/to_run/ted_min5.dict', 'rb'), encoding='utf8')
 max_seq_len = 50
@@ -64,96 +66,89 @@ class Net(nn.Module):
 
 # net = torch.load('diagnostic-classifier.pt', map_location=lambda storage, loc: storage)
 
-net = Net()
-optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum)
-ce_loss = nn.CrossEntropyLoss()
-# function to transform sentence into word id's and put them in a pytorch Variable
-# NB Assumes the sentence is already tokenised!
-def tokenise(sentence, dictionary):
-    words = sentence.split(' ')
-    l = len(words)
-    assert l <= max_seq_len, "Sentence too long"
-    token = 0
-    ids = torch.LongTensor(l)
+class Trainer:
+    def __init__(self, model, dictionary):
+        # initialize vars for training the net
+        self.net = Net()
+        self.optimizer =  optim.SGD(self.net.parameters(), lr=args.lr, momentum=args.momentum)
+        self.loss = nn.CrossEntropyLoss()
+       
+        # store the model and some utility variables
+        self.model = model
+        self.dictionary = dictionary
+        self.ntokens = len(dictionary)
+        self.hidden = self.model.init_hidden(1)
 
-    for word in words:
-        try:
-            ids[token] = dictionary.word2idx[word]
-        except KeyError:
-            print('Unknown token')
-            ids[token] = dictionary.word2idx['<unk>']
-        token += 1
-    return ids
+        # Turn on evaluation mode which disables dropout.
+        self.model.eval()
+    
+    def get_id(self, word):
+        return dictionary.word2idx[word] if word in self.dictionary.word2idx else self.dictionary.word2idx['<unk>']
 
-def evaluate(model, dictionary, sentence, index):
-    # Turn on evaluation mode which disables dropout.
-    model.eval()
+    # function to transform sentence into word id's and put them in a pytorch Variable
+    # NB Assumes the sentence is already tokenised!
+    def tokenise(self, sentence):
+        words = sentence.split(' ')
+        assert len(words) <= max_seq_len, "Sentence too long"
+        return torch.LongTensor([self.get_id(w) for w in words])
+    
+    def evaluate(self, sentences, targets):
+        # storing the output here
+        outputs, hiddens = [], []
 
-    # number of tokens (= output size)
-    ntokens = len(dictionary)
-    hidden = model.init_hidden(1)
+        # make the optimizer zero
+        for sentence, target in zip(sentences, targets):
+            # set optimizer to zero & get the input for it
+            self.optimizer.zero_grad()
+            input_data = Variable(self.tokenise(sentence))
 
-    # tokenise the sentence, put in torch Variable
-    test_data = tokenise(sentence, dictionary)
-    input_data = Variable(test_data)
+            # Run the model, compute probabilities by applying softmax
+            output, hidden = self.model(input_data, self.hidden)
 
-    optimizer.zero_grad()
+            # the prediction in the hidden layer
+            prediction = self.net(hidden[0])[1]
+            target_id = pos_tags.get(pos_dict.get(target))
+            
+            # append to the output and hiddens (faux batching)
+            outputs.append(output)
+            hiddens.append(hidden)
+    
+            # some user output
+            print('%s => %s' % (sentence, target))
+            if target_id == None:
+                print(" ---> skipped last sentence, because target \"%s\"not in dictionary" % target)
+                continue
 
-    # Run the model, compute probabilities by applying softmax
-    output, hidden = model(input_data, hidden)
+            # make the loss and optimizer step
+            loss = self.loss(prediction, Variable(torch.LongTensor([target_id])))
+            loss.backward()
+            self.optimizer.step()
+        
+        # return the output
+        return outputs, hiddens
 
-    prediction = net(hidden[0])[1]
-    temp = Variable(torch.LongTensor(1))
-    target = pos_tags.get(pos_dict.get(target_words[index]))
-    if target == None:
-        print('Skipped sentence')
-        return output, hidden
-    temp[0] = target
-    print('Target', temp)
-    loss = ce_loss(prediction, temp)
-    loss.backward()
-    optimizer.step()
-
-    output_flat = output.view(-1, ntokens)
-    logits = output[-1, :]
-    sm = softmax(logits).view(ntokens)
-
-    def get_prob(word):
-        try:
-            return sm[dictionary.word2idx[word]].data[0]
-        except KeyError:
-            return 0
-
-    # print('\n'.join(
-    #     ['%s: %f' % (word, get_prob(word)) for word in check_words]
-    # ))
-
-    return output, hidden
-#
 def plot_tensor(tensors, indices):
     plt.imshow(tensors[:, indices])
     plt.colorbar()
     plt.show()
 
-for index, sentence in enumerate(sentences):
-    print(index, '/', len(sentences))
-    output, hidden = evaluate(lm, dictionary, sentence, index)
-    # Visualize hidden layer
-    # indices = [dictionary.word2idx[word] for word in check_words]
-    # img_data = output.view(-1, len(dictionary)).data.numpy()
+# create a trainer and set batch size
+trainer = Trainer(lm, dictionary)
 
+# loop over the batches
+for i in range(0, len(sentences), args.batch_size):
+    print(i + 1, '/', len(sentences))
+    outputs, hiddens = trainer.evaluate(sentences[i:i+args.batch_size], target_words[i:i+args.batch_size])
+
+# save our own neural net
 torch.save(net, 'diagnostic-classifier.pt')
 
 print('Evaluating diagnostic classifier...')
 
-for index, sentence in enumerate(sentences[:10]):
-    _, hidden = evaluate(lm, dictionary, sentence, index)
-    prediction = net(hidden[0]).data.numpy().tolist()[0]
+for i in range(0, len(sentences), batch_size):
+    _, hiddens = trainer.evaluate(sentences[i:i+args.batch_size], target_words[i:i+args.batch_size])
+    prediction = trainer.net(trainer.hidden[0]).data.numpy().tolist()[0]
     print('Word to predict:', target_words[index])
     print('Training sentence:', sentence)
     print('Predicted POS tag:', get_pos_tag(prediction[0].index(max(prediction[0]))))
     print('-------------------------')
-
-# print('Tensor dimensions:', img_data.shape)
-
-# plot_tensor(img_data, indices)
